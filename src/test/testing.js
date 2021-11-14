dbcc200606211a22e141c5b2fd76f6c4dc7cc8bf
DESKTOP-UKPOCOD\khlam <khlamat@connect.ust.hk>
2021-11-14 16:52:25 +0800

    add increase the test case size

--- src/test/testing.js
+++ src/test/testing.js
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import solver from '../solver/solver.js';
 import { createRequire } from "module";
+let boardSize = 4;
 const require = createRequire(import.meta.url);
-const testCases = require("./testCases.json")
+const testCases = require(`./testCases${boardSize}.json`)
 let results = []
 for(let i = 0; i < testCases.length; i++){
     results[i] = {};
@@ -15,12 +16,10 @@ for(let i = 0; i < testCases.length; i++){
 
 const jsonContent = JSON.stringify(results);
 
-fs.writeFile("./testCasesResults.json", jsonContent, 'utf8', function (err) {
+fs.writeFile(`./testCasess${boardSize}Results.json`, jsonContent, 'utf8', function (err) {
     if (err) {
         return console.log(err);
     }
 
     console.log("The file was saved!");
 }); 
-
-// console.log(results)
